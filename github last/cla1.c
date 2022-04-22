53895a15263d5742170cf3baefe17e0450882c5c
Ayush Garg <humbleayush30@gmail.com>
2022-04-22 21:42:29 +0530

    Four Variable Code

--- github last/cla1.c
+++ github last/cla1.c
@@ -5,6 +5,6 @@ scanf("%d %d %d %d",&a,&b,&c,&d);
 printf("Addition-: a+b+c+d = %d\n",a+b+c+d);
 printf("Subtraction-: a-b-c-d = %d\n",a-b-c-d);
 printf("Multiplication-: a*b*c*d = %d\n",a*b*c*d);
-printf("Division-: (a/b)*(c/d) = %d",c/d);
+printf("Division (with multiplication)-: (a/b)*(c/d) = %d",c/d);
 	return 0;
 }
